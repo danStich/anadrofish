bf78f9a6f4aa12850345a3c0042e4cfe188aeb74
danStich <daniel.stich@oneonta.edu>
2019-05-12 14:51:31 -0400

    add example for beverton_holt()

---
+++ inst/examples/bevholt_ex.R
@@ -0,0 +1,19 @@
+# Example usage
+\dontrun{
+  
+# Run Beverton-Holt model to predict number
+# of age-0 recruits in freshwater under 
+# varying habitat amount with 100 fish and
+# default parameter settings.
+  
+# Start by making a sequence of habitat amounts (acres)
+  acres = seq(0, 100, 1)
+  
+# Run the Beverton-Holt curve for 100 individuals
+# in varying habitats with default a and b parameters
+  recruits = beverton_holt(S=100, acres = seq(0,1000,10))
+  
+# Plot the results
+  plot(x = acres, y = recruits, type='l')
+  
+}
