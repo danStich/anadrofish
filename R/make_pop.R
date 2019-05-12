a9dad1de415cae42cc1b867891c18fb2c7781442
danStich <daniel.stich@oneonta.edu>
2019-05-12 15:20:26 -0400

    update documentation

--- R/make_pop.R
+++ R/make_pop.R
@@ -1,25 +1,39 @@
 #' @title make_pop
 #'
-#' @description Function used to seed initial
-#' populations for simulation based on life-history
-#' characteristics and stable age distributions
+#' @description Function used to seed initial age-structured
+#' population for simulation based on life-history
+#' characteristics. Uses an initial population seed, mortality
+#' estimates, and reproductive rate to populate a leslie 
+#' matrix and extract stable age distribution through
+#' eigen analysis with functions imported from the \code{demogR} package 
+#' (Holland Jones 2007).
 #'
 #' @param max_age The maximum age of fish in the population(s). A numeric vector of length 1.
 #'
-#' @param nM Instantaneous natural mortality rate. A numeric vector of length 1.
+#' @param nM Instantaneous natural mortality rate. 
+#' A numeric vector of length 1.
 #'
-#' @param fM Instantaneous fishing mortality rate. A numeric vector of length 1.
+#' @param fM Instantaneous fishing mortality rate. 
+#' A numeric vector of length 1.
 #'
 #' @param n_init Initial population abundance (includes all age classes)
+#' 
+#' @param f Reproductive rate (rate of replacement).
 #'
-#' @return A vector containing age-specific abundances. A numeric vector with length = max_age.
+#' @return A vector containing age-specific abundances 
+#' with \code{length = max_age}. 
 #'
-#' @references None yet.
+#' @example inst/examples/makepop_ex.R
+#'
+#' @references J. Holland Jones. 2007. demogR: A package for the construction
+#' and analysis of age-structured demographic models. Journal of Statistical
+#' Sofware 22(10):1-28. URL: http://dx.doi.org/10.18637/jss.v022.i10.
 #'
 #' @export
 #'
 
-make_pop <- function(max_age = 11, nM = 0.38, fM = 0.0, n_init=1e5){
+make_pop <- function(max_age, nM, fM, n_init,
+                     f = c(rep(0, 2), rep(1, max_age-2))){
 
   # Calculate total mortality
     Z <- nM + fM
@@ -27,23 +41,15 @@ make_pop <- function(max_age = 11, nM = 0.38, fM = 0.0, n_init=1e5){
   # Survival rate
     s <- rep((1-exp(-Z)), max_age)
 
-  # Fecundity
-  ### Needs some thought
-  ### Could initialize here and then have user-defined
-  ### values in a later function?
-
-  # Reproductive rate
-    f <- c(rep(0, 2), rep(1, max_age-2))
-
   # Make a Leslie matrix for projection
-    les <- leslie.matrix(
+    les <- demogR::leslie.matrix(
       lx=cumprod(s),
       mx=f
       )
 
   # Calculate asymptotic growth
   # rate and related quantities
-    dem <- eigen.analysis(les)
+    dem <- demogR::eigen.analysis(les)
 
   # Multiply stable age dist
   # by an arbitrarily large
---
+++ inst/examples/makepop_ex.R
@@ -0,0 +1,7 @@
+# Example usage
+\dontrun{
+  
+# Simulate a fish population at stable age distribution
+  pop = make_pop(max_age = 9, nM = 0.38, fM = 0.05, n_init = 1e6)
+  
+}
