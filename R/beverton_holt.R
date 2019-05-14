223bd52e75a10d857cbd6eaa9f227f74a1d8e95f
danStich <daniel.stich@oneonta.edu>
2019-05-14 07:47:34 -0400

    update documentation

--- R/beverton_holt.R
+++ R/beverton_holt.R
@@ -2,8 +2,8 @@
 #'
 #' @description Beverton and Holt (1957) stock-recruit curve 
 #' with habitat constraints. Default parameter values are tuned to predict 
-#' age-0 recruitment assuming a carrying capacity for spawning adults of 
-#' 100 fish per acre. See "\bold{Uses}" below.
+#' age-1 recruitment assuming a carrying capacity for spawning adults of 
+#' 100 fish per acre. See \code{details}.
 #'
 #' @param a Ratio of recruits per spawner. Density independent parameter 
 #' equivelant to the slope near \code{S = 0}. By default specified as 
@@ -24,24 +24,23 @@
 #' density-dependent recruitment from the Beverton-Holt curve. If \code{TRUE}
 #' then a vector of \code{length} = max age must be passed to \code{S}.
 #'
-#' @details The primary use of this function in the \code{anadrofish} package
-#' is to predict number of age-0 recruits in a river based on the number of
+#' @details The primary use of this function in the \code{anadrofish} package 
+#' is to predict number of age-1 recruits in a river based on the number of 
 #' fish within a functional habitat unit, and the number of surface acres 
-#' of habitat in the same unit.However, the number of potential uses is
-#' flexible. For example, passing a single value to each argument would 
-#' result calculation of a single recruitment value. But, passing a vector
-#' to one of the four default arguments allows the user to explore values
-#' of recruitment over a range of each input. 
+#' of habitat in the same unit. However, the number of potential uses is 
+#' flexible. Passing a single value to each argument would 
+#' result in calculation of a single recruitment value. Alternatively, 
+#' passing a vector to one of the default arguments allows the user to 
+#' explore values of recruitment over a range of each input. 
 #' 
 #' Under the current implementation, it is recommended that the user
 #' pass multiple values to only a single argument per call.
 #' Otherwise, we recommend conducting more in-depth sensitivity 
 #' explorations by use of single values applied 
-#' under a boot-strapping, or Monte Carlo approach using this function.
+#' under a boot-strapping, or Monte Carlo approach.
 #'
 #' @return A numeric vector with one or more elements representing 
-#' age-0 recruits (downstream migrants in freshwater) per unit of 
-#' habitat
+#' age-1 recruits (downstream migrants in freshwater).
 #'  
 #' @example inst/examples/bevholt_ex.R
 #'
