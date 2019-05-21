c8f2c9586839f16bcd8ebcf9f899917d9a09281b
danStich <daniel.stich@oneonta.edu>
2019-05-21 14:01:13 -0400

    add functions and documentation

---
+++ R/make_habitat.R
@@ -0,0 +1,39 @@
+#' @title Project population
+#'
+#' @description Function used to make habitat from the built-in 
+#' dataset(s)
+#'
+#' @param habitat_data A built-in \code{data.frame} containing habitat 
+#' estimates (km2) for the scenario and region selected.
+#'
+#' @param river Character string specifying river name
+#' 
+#' @param type Character string indicating the type of habitat 
+#' to be considered. Possible values include \code{functional}, 
+#' or \code{segment}. For \code{PassageToHabitat < 1}, 
+#' \code{type = 'functional'} adjusts \code{habitatSegment_sqkm} 
+#' proportionally and uses the variable \code{functional_habitatSegment_sqkm} 
+#' for calculations. See \code{?habitat} for explanation of built-in 
+#' data sets.
+#' 
+#' #@example inst/examples/makefec_ex.R
+#' 
+#' @export
+#'
+make_habitat <- function(habitat_data, river, type = 'functional'){
+  
+  # Select habitat units based on HUC 10 watershed names
+  units <- habitat[grep(pattern = river, x = habitat$HUC10_location),]
+  
+  # Calculate habitat surface acres from the 
+  # sum of functional habitat in the subset
+  
+  if(type == 'functional'){
+    acres <- 247.105 * sum(units$functional_habitatSegment_sqkm)
+  } else {
+    stop('Error: only functional habitat implemented so far!')
+  }
+  
+  return(acres)
+  
+}
---
+++ R/project_pop.R
@@ -0,0 +1,43 @@
+#' @title Project population
+#'
+#' @description Function used to project population forward 
+#' one time step using matrix multiplication. Based on 
+#' modification of the \code{project.leslie} in \code{demogR}
+#'
+#' @param x An numeric vector containing age-specific 
+#' abundance at end of year t.
+#' 
+#' @param age0 A vector of length one containing number of new recruits 
+#' at end of year t.
+#' 
+#' @param nM Instantaneous natural mortality.
+#' 
+#' @param fM Instantaneous fishing mortality.
+#'
+#' @return A numeric vector of age-structured abundance in time t + 1
+#'
+#' #@example inst/examples/makefec_ex.R
+#' 
+#' @export
+#'
+project_pop <- function(x, age0, nM, fM){
+  
+  # Calculate total mortality
+    Z <- nM + fM
+
+  # Survival rate
+    s <- rep(1-(1-exp(-Z)), max_age+2)  
+      
+  # Make the projection matrix
+    les <- demogR::leslie.matrix(
+      lx = cumprod(c(s)),
+      mx = rep( 1, length(s) )
+      )  
+  
+  # Multiply eggs (fecundity) by sex ratio and hatch success
+    tplus1 <- les %*% c(age0, x)[1:(length(c(age0, x)))]
+  
+  # Return the result to R
+    return(tplus1[2:length(tplus1)])
+
+}
