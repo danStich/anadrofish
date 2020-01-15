0b5695e79a906720d290ac73f73a6960ddee67aa
danStich <36766897+danStich@users.noreply.github.com>
2020-01-14 22:50:42 -0500

    add regional vbgf parameters

---
+++ R/vbgf_SI.R
@@ -0,0 +1,22 @@
+#' @title Sex-aggregate von Bertalanffy growth parameters for 
+#' Southern Iteroparous American shad
+#'
+#' @description A dataset containing sex-aggregate estimates
+#' of von Bertalanffy growth parameters for Southern 
+#' Iteroparous life-history region. Data frame contains posterior
+#' draws from coast-wide growth models (Gilligan 2019).
+#'
+#' @format A data frame with 1500 observations of 3 variables:
+#' \describe{
+#' 
+#' \code{Linf}{ Asymptotic length of fish}
+#' 
+#' \code{K}{ Brody growth coefficient}
+#' 
+#' \code{t0}{ Size at age zero}
+#' }
+#' 
+#' @source Gilligan, E. K. 2019. Understanding influences of climate change
+#' on American Shad recovery, fisheries management, and dam impacts. Master's
+#' Thesis, SUNY Oneonta, Oneonta, NY, USA.
+"vbgf_SI"
