e758bbf93ebbc4befe447f6a7f69d54e91dbba0d
danStich <36766897+danStich@users.noreply.github.com>
2020-01-17 15:03:31 -0500

    add Fecundity-Weight relationships from Olney and McBride (2003)

---
+++ R/olney_mcbride.R
@@ -0,0 +1,22 @@
+#' @title Weight-batch fecundity relationships for American Shad
+#'
+#' @description A dataset containing weight-batch fecundity relationships
+#' for St. Johns, York, and Connecticut Rivers from Olney and McBride (2003).
+#' Regressions fit log10 batch fecundity as a function of
+#' log10 body weight (g).
+#' 
+#' @format A data frame with 3 observations of 4 variables:
+#' \describe{
+#' 
+#' \code{system}{ Study system}
+#' 
+#' \code{region}{ Life-history region: NI = Northern iteroparous,
+#' SI = Southern iteroparous, SP = Semelparous}
+#' 
+#' \code{alpha}{ Intercept}
+#' 
+#' \code{beta}{ Slope}
+#' }
+#' 
+#' @source Olney and McBride (2003)
+"olney_mcbride"
