22db9d79cc983ad05433cda709348d32a70e49ce
danStich <36766897+danStich@users.noreply.github.com>
2020-02-03 18:41:45 -0500

    added juvenile mortality info and docs

---
+++ R/crecco_1983.R
@@ -0,0 +1,28 @@
+#' @title American shad habitat
+#'
+#' @description A built-in dataset containing estimated daily 
+#' mortality rates for larval and juvenile American shad in the
+#' Connecticut River, USA 1979-1982 (Crecco et al. 1983).
+#' 
+#' @format A data frame with 24 observations of 6 variables:
+#' \describe{
+#' \code{Year}{ Year of study}
+#' 
+#' \code{Age}{ Fish age (d)}
+#' 
+#' \code{Md}{ Daily mortality}
+#'
+#' \code{Sd}{ Daily survival}
+#'
+#' \code{Si}{ Interval survival}
+#'
+#' \code{Sc}{ Cumulative survival}
+#'
+#' }
+#' 
+#' #' @references Crecco, V., T. Savoy, and L. Gunn. 1983. Daily mortality rates 
+#' of larval and juvenile American shad (*Alosa sapidissima*) in the Connecticut 
+#' River with changes in year-class strength. Canadian Journal of 
+#' Fisheries and Aquatic Sciences 40:1719–1728.
+#' 
+"crecco_1983"
---
+++ R/sim_juvenile_s.R
@@ -0,0 +1,28 @@
+#' @title Juvenile (hatch-to-outmigrant) survival
+#'
+#' @description Function used to simulate juvenile (hatch-to-outmigrant) 
+#' survival from daily rates reported by Crecco et al. (1983) from
+#' the Connecticut River, USA during 4 consecutive years.
+#'
+#' @param crecco_1983 built-in dataset containing daily mortality 
+#' rates, daily survival, and cumulative survival during varying
+#' post-hatch periods. See \code{\link{crecco_1983}}.
+#' 
+# #' @example inst/examples/make_postspawn_ex.R
+#'
+#' @references Crecco, V., T. Savoy, and L. Gunn. 1983. Daily mortality rates 
+#' of larval and juvenile American shad (*Alosa sapidissima*) in the Connecticut 
+#' River with changes in year-class strength. Canadian Journal of 
+#' Fisheries and Aquatic Sciences 40:1719–1728.
+#' 
+#' @export
+#'
+sim_juvenile_s <- function(crecco_1983){
+  
+  jmean <- log(mean(crecco_1983$Sc[crecco_1983$Age==70]))
+  jsd <- sd(crecco_1983$Sc[crecco_1983$Age==70])*10
+  
+  juvenile_s <- exp(rnorm(1, jmean, jsd))
+  return(juvenile_s)
+  
+}
