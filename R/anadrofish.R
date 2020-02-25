8b16eb652142d443ff24a0864642e63ad3439d9a
danStich <36766897+danStich@users.noreply.github.com>
2020-02-24 22:14:47 -0500

    update package docs, DESCRIPTION, and NAMESPACE

--- R/anadrofish.R
+++ R/anadrofish.R
@@ -2,23 +2,44 @@
 #' 
 #' The anadrofish package is a collection of tools for running
 #' coastwide or river-specific population models for
-#' anadromous fish in response to habitat change.
+#' anadromous fish in response to habitat change from dams.
 #' 
 #' @section Functions called directly:
 #'  \describe{
 #'    \code{\link{beverton_holt}} \cr
+#'    \code{\link{get_rivers}} \cr
 #'    \code{\link{lower95}} \cr 
-#'    \code{\link{make_fec}} \cr
-#'    \code{\link{make_habitat}}
+#'    \code{\link{make_downstream}} \cr
+#'    \code{\link{make_habitat}} \cr
+#'    \code{\link{make_iteroparity}} \cr
+#'    \code{\link{make_lat}} \cr
+#'    \code{\link{make_maxage}} \cr
+#'    \code{\link{make_mortality}} \cr
 #'    \code{\link{make_pop}} \cr
+#'    \code{\link{make_postspawn}} \cr
+#'    \code{\link{make_recruits}} \cr
 #'    \code{\link{make_spawners}} \cr
+#'    \code{\link{make_spawnrecruit}} \cr
 #'    \code{\link{project_pop}} \cr
+#'    \code{\link{sim_juvenile_s}} \cr
+#'    \code{\link{sim_pop}} \cr
 #'    \code{\link{upper95}} \cr
 #'  }
 #'  
 #' @section Data:
 #'   \describe{
+#'     \code{\link{crecco_1983}} \cr
 #'     \code{\link{habitat}} \cr
+#'     \code{\link{inventory}} \cr
+#'     \code{\link{length_weight}} \cr
+#'     \code{\link{maturity}} \cr
+#'     \code{\link{max_ages}} \cr
+#'     \code{\link{mortality}} \cr
+#'     \code{\link{olney_mcbride}} \cr
+#'     \code{\link{shad_rivers}} \cr
+#'     \code{\link{vbgf_NI}} \cr
+#'     \code{\link{vbgf_SI}} \cr
+#'     \code{\link{vbgf_SP}} \cr
 #'   }
 #'
 #'
@@ -26,7 +47,7 @@
 #' 
 #' @name anadrofish
 #' 
-#' @importFrom demogR leslie.matrix eigen.analysis
+#' @importFrom truncnorm rtruncnorm
 #' @importFrom stats quantile
 #' 
 NULL
